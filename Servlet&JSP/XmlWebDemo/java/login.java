2114e62966fb5065c88b287d698a0894416d5179
Raj <120565750+Raj2342@users.noreply.github.com>
2024-08-06 22:56:06 +0530

    Add files via upload

---
+++ Servlet&JSP/XmlWebDemo/java/login.java
@@ -0,0 +1,19 @@
+package in.sp.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class login   extends  HttpServlet
+{
+    
+     @Override
+    protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	System.out.println("i am in service method .....");
+    }
+	    
+}
